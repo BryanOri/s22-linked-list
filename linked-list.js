0978774d2e45e11a3ef3dd47ff8a6f277959ce4c
Jonathan C <jsea@uw.edu>
2022-01-20 16:39:21 -0800

    Assignment for creating a very basic LinkedList class (only pushing & popping from the back)

---
+++ linked-list.js
@@ -0,0 +1,18 @@
+// Step 1: Create a Node class that holds a generic "value" variable
+//         and a "next" variable to hold the next Node in the List
+
+
+
+// Step 2: Create a LinkedList class that holds a "head" variable to
+//         represent the first Node in the list
+
+
+
+// Step 3: Add a "getNode" function to the LinkedList class that will
+//         retrieve a value from the LinkedList by index
+//
+// Step 4: Add a "pushBack" function to the LinkedList class that will
+//         add a new Node to the end of the Linked List
+//
+// Step 5: Add a "popBack" function to the LinkedList class that will
+//         remove an existing Node from the end of the Linked List
