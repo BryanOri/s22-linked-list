99bc2df4f51c13c86ef013b41b62eb217967c215
Jonathan C <jsea@uw.edu>
2022-01-21 18:08:09 -0800

    Updated instructions to be clearer to understand

--- linked-list.js
+++ linked-list.js
@@ -1,18 +1,24 @@
-// Step 1: Create a Node class that holds a generic "value" variable
-//         and a "next" variable to hold the next Node in the List
+// Step 1: Create a ListNode class that holds a generic "value"
+//         variable and a "next" variable to hold the next ListNode in
+//         the Linked List
 
 
 
 // Step 2: Create a LinkedList class that holds a "head" variable to
-//         represent the first Node in the list
+//         represent the first ListNode in the Linked List and a "tail"
+//         variable to represent the last ListNode in the Linked List.
+//         Keep in mind, these will need to be updated if the first
+//         ListNode or the last ListNode in the Linked List ever
+//         changes.
 
 
 
-// Step 3: Add a "getNode" function to the LinkedList class that will
-//         retrieve a value from the LinkedList by index
+// Step 3: Add a "getNodeValue" function to the LinkedList class that
+//         will retrieve a ListNode's "value" from the Linked List by
+//         the ListNode's index in the Linked List
 //
 // Step 4: Add a "pushBack" function to the LinkedList class that will
-//         add a new Node to the end of the Linked List
+//         add a new ListNode to the end of the Linked List
 //
 // Step 5: Add a "popBack" function to the LinkedList class that will
-//         remove an existing Node from the end of the Linked List
+//         remove an existing ListNode from the end of the Linked List
